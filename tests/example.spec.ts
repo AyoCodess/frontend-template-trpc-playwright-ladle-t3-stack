e5083514f3d3f85bbc5f4b6e61a567cdfc4f2cd8
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-04-17 10:41:57 +0200

    added url for testing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import { BASE_URL } from "./constants";
 
 test("has title", async ({ page }) => {
-  await page.goto(process.env.NEXT_PUBLIC_BASE_URL ?? BASE_URL);
+  await page.goto(BASE_URL);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Frontend Template/);
