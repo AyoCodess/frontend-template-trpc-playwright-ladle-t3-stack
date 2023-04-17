314956cc43c4dd3ac10a440f5799e5726cbb4ab4
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-04-17 08:43:16 +0200

    fixed tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("http://localhost:3005/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Create T3 App/);
+  await expect(page).toHaveTitle(/Frontend Template/);
 });
