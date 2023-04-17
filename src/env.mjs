32cfc3032eaf577e27c0bc29bb60dc2173bc0edc
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-04-17 09:59:01 +0200

    added base url constant

--- src/env.mjs
+++ src/env.mjs
@@ -13,6 +13,7 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object({
+  NEXT_PUBLIC_BASE_URL: z.string().min(1),
   // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
 });
 
@@ -23,6 +24,7 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
+  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
@@ -54,7 +56,7 @@ if (!skip) {
   if (parsed.success === false) {
     console.error(
       "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors,
+      parsed.error.flatten().fieldErrors
     );
     throw new Error("Invalid environment variables");
   }
@@ -68,7 +70,7 @@ if (!skip) {
         throw new Error(
           process.env.NODE_ENV === "production"
             ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`,
+            : `❌ Attempted to access server-side environment variable '${prop}' on the client`
         );
       return target[/** @type {keyof typeof target} */ (prop)];
     },
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from "@playwright/test";
+import { BASE_URL } from "./constants";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:3005/");
+  await page.goto(process.env.NEXT_PUBLIC_BASE_URL ?? BASE_URL);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Frontend Template/);
